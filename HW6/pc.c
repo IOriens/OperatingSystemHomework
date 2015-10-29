e68eba7e18b7c80436d9d7aad8cb67afa6433bc1
Oriens <812561857@qq.com>
2015-10-29 20:08:20 +0800

    Add comment and change the algorithm
    Add comment and change the algorithm of insertion and removement

--- HW6/pc.c
+++ HW6/pc.c
@@ -4,44 +4,40 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-
-
+//定义缓存区大小
 typedef int buffer_item;
 #define BUFFER_SIZE 5
 
-
+//定义互斥锁和empty、full信号量
 pthread_mutex_t mutex;
-
 sem_t empty;
 sem_t full;
 
-
+//定义缓冲区
 buffer_item buffer[BUFFER_SIZE];
-int head=0,end=0;
+buffer_item front=0,rear=0;
 
+
+//插入物品
 int insert_item(buffer_item item){
 	int i=0;
-	for(i=0;i<BUFFER_SIZE;i++){
-		if(buffer[i]==0){
-			buffer[i]=item;
-			return 0;
-		}
-	}
-	return -1;
+	if((rear+1)%BUFFER_SIZE==front)
+		return -1;
+	buffer[rear]=item;
+	rear=(rear+1)%(BUFFER_SIZE);
+	return 0;
 }
 
+//移除物品
 int remove_item(buffer_item *item){
-	int i=0;
-	for(i=0;i<BUFFER_SIZE;i++){
-		if(buffer[i]>0){
-			*item=buffer[i];
-			buffer[i]=0;
-			return 0;
-		}
-	}
-	return -1;
+	if(front=rear)
+		return -1;
+	*item=buffer[front];
+	front=(front+1)%BUFFER_SIZE;
+	return 0;
 }
 
+//生产者
 void *producer(void *param){
 	buffer_item item;
 	srand(time(NULL));
@@ -61,6 +57,7 @@ void *producer(void *param){
 	}
 }
 
+//消费者
 void *consumer(void *param){
 	buffer_item item;
 	srand(time(NULL));
@@ -82,34 +79,42 @@ void *consumer(void *param){
 
 int main(int argc,char *argv[]){
 
-	if(argc<4){
-		printf("Use more arguments\n");
+	//判断输入参数个数是否符合要求
+	if(argc != 4){
+		printf("Use Correct arguments\n");
 		return 0;
 	}
 
+	//解析传入的参数
 	int sleeptime=atoi(argv[1]); 
 	int pthnum=atoi(argv[2]);
 	int cthnum=atoi(argv[3]);
 
+	//输出解析的参数
 	printf("Sleep Time:%d\n",sleeptime);
 	printf("Producer:%d,Consumer:%d\n",pthnum,cthnum);
 
+	//初始化缓冲区
 	int i=0;
+	buffer_item initi=0;
 	for(i=0;i<BUFFER_SIZE;i++){
-		buffer[i]=0;
+		buffer[i]=initi;
 	}
 
+	//定义生产者消费者线程数组
 	pthread_t pth[pthnum+10];
 	pthread_t cth[cthnum+10];
 
+	//初始化多线程属性
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 
-
+	//初始化信号量以及互斥锁
 	sem_init(&empty,0,5);
 	sem_init(&full,0,0);
 	pthread_mutex_init(&mutex,NULL);
 
+	//创建多个线程
 	int j;
 	for(i=0;i<pthnum;i++){
 		pthread_create(&pth[i],&attr,producer,NULL);
@@ -121,6 +126,7 @@ int main(int argc,char *argv[]){
 		printf("--Consumer thread %d created!\n", j);
 	}
 
+	//使主进程进入睡眠状态
 	sleep(sleeptime);
 	return 0;
 }
