29957149f2aa33bb35db31115b6ce3b8f9c6013f
oriens <812561857@qq.com>
2015-10-31 06:05:12 -0700

    experiment2 finished

---
+++ experiment2/messageQueue.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <sys/msg.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 100
+
+struct msgbuff
+{
+	long mtype;
+	char data[MAX];
+};
+
+int main(){
+	int pid1,pid2;
+
+	key_t key;
+	if((key=ftok("/tmp",'g'))<0){
+		printf("Getting key error!\n");
+		return -1;
+	}
+
+	int msgid;
+
+	if((msgid=msgget(key,IPC_CREAT|0666))==-1){
+		printf("msg queue create error\n");
+		return -1;
+	}
+
+	pid1=fork();
+	if(pid1==0){
+		printf("Welcome to child process 1\n Sending the message.....\n");
+		sleep(1);
+		struct msgbuff msg1;
+		msg1.mtype=getppid();
+		strcpy(msg1.data,"ok");
+		if(msgsnd(msgid,&msg1,sizeof(msg1.data),0)<0){
+			printf("Sending error\n");
+			exit(1);
+		}else{
+			FILE *fp;
+			fp=fopen("123.txt","w");
+			fprintf(fp,"Text in 123.txt");
+			fclose(fp);
+			exit(0);
+		}
+	}else{
+		
+		pid2=fork();
+		wait(NULL);
+	}
+
+	if(pid2==0){
+		printf("Welcome to child process 2\n Receiving the message....\n");
+		sleep(3);
+		struct msgbuff msg2;
+		if(msgrcv(msgid,&msg2,MAX,getppid(),0)<0){
+			printf("Receiving error!!!\n");
+			exit(1);
+		}else{
+			printf("Receiving complete\n");
+			printf("The message is : %s \n",msg2.data);
+			if(strcmp(msg2.data,"ok")==0){
+				char ch;
+				FILE *fp;
+				fp=fopen("123.txt","r");
+				while((ch = fgetc(fp)) != EOF)
+					putchar(ch);
+				printf("\n");
+				fclose(fp);	
+			}
+		}
+		exit(0);
+	}else{
+		wait(NULL);
+	}
+	return 0;
+}
